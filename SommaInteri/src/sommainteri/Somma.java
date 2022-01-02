d7d50ff43fcdd000e3199576f9a7808668026b07
salvatoregrima <grimaldisalvatoreinginf@gmail.com>
2022-01-02 17:23:06 +0100

    refactor 1

---
+++ SommaInteri/src/sommainteri/Somma.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sommainteri;
+
+/**
+ *
+ * @author Salvatore
+ */
+public class Somma {
+    
+    private Integer a;
+    
+    private Integer b;
+
+    public Somma() {
+    }
+
+    public Integer getA() {
+        return a;
+    }
+
+    public Integer getB() {
+        return b;
+    }
+
+    public void setA(Integer a) {
+        this.a = a;
+    }
+
+    public void setB(Integer b) {
+        this.b = b;
+    }
+    
+    
+    
+}
